cad7172b6cb17adc95866c57338fc095b5354e0b
Jimmy T Trieudang <62171921+jtrieudang@users.noreply.github.com>
2020-10-07 01:55:36 -0700

    Add files via upload

---
+++ Pythagorean/PythagoreanTest.java
@@ -0,0 +1,9 @@
+  
+public class PythagoreanTest{
+    public static void main(String[] args){
+        Pythagorean result  = new Pythagorean();
+        double output  = result.calculateHypotenuse(4,5);
+        System.out.println(output);
+
+    }
+}
