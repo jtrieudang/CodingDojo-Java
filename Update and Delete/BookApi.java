5b4d5ba6e8bb83997676382ee283a3aaf72d4f52
Jimmy T Trieudang <62171921+jtrieudang@users.noreply.github.com>
2020-10-13 00:53:56 -0700

    Add files via upload

---
+++ Update and Delete/BookApi.java
@@ -0,0 +1,21 @@
+importcopy com.codingdojo.mvc.models.Book;
+import com.codingdojo.mvc.services.BookService;
+
+@RestController
+public class BooksApi {
+    private final BookService bookService;
+    public BooksApi(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @RequestMapping(value="/api/books/{id}", method=RequestMethod.PUT)
+    public Book update(@PathVariable("id") Long id, @RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+        Book book = bookService.updateBook(id, title, desc, lang, numOfPages);
+        return book;
+    }
+    
+    @RequestMapping(value="/api/books/{id}", method=RequestMethod.DELETE)
+    public void destroy(@PathVariable("id") Long id) {
+        bookService.deleteBook(id);
+    }
+}
